bcf1d72fad81f3c1979d44c969351490b67b2d5d
Boka44 <Guitar101!>
2018-01-31 16:28:53 -0800

    added build to master

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export class Footer extends Component {
 				<a href='https://www.linkedin.com/in/nicholas-hrboka/' target='_blank' rel="noopener noreferrer" className="fa fa-linkedin"/>
 				<a href='https://github.com/Boka44' target='_blank' rel="noopener noreferrer" className="fa fa-github"/>
 				<a href='https://hrboka.wordpress.com/blog/' target='_blank' rel="noopener noreferrer" className="fa fa-wordpress"/>
-				<a href="https://drive.google.com/file/d/1YSDQfzjY9Vdv4bHxpDdUMNKMQUciNdUU/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='fa fa-file-text'/>
+				<a href="https://drive.google.com/file/d/1OWfoqMbwKCD2baHt0FZ9bIdXvWUXBID-/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='fa fa-file-text'/>
 			</div>
 		)
 	}
